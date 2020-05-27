b4b694e33f89c199a9445ff9eb7fe6541dd59cb5
Valeri Stanchev <stan4ev6@gmail.com>
2020-05-27 15:15:48 +0300

    Added comments to Table header

--- Table.h
+++ Table.h
@@ -3,30 +3,75 @@
 
 #include "IColumn.h"
 
+/// \brief Table class
 class Table {
     private:
-        std::string name;
-        std::string file;
-        std::vector<IColumn*> columns;
+        std::string name; ///< name of the table
+        std::string file; ///< file in which the table is saved
+        std::vector<IColumn*> columns; ///< columns in the table constructing the rows
 
     public:
+        /// \brief Constructor with 2 parameters
+        /// \param first 'string' which is the name of the table
+        /// \param second 'string' which is the file from which the table is loaded
         Table(const std::string&, const std::string&);
+
+        /// \brief Destructor as we have dynamic memory
         ~Table();
 
+        /// \brief Getter for table name
+        /// \return 'string' which is the name of the table
         std::string getName() const;
-        std::string getFilePath() const;
+
+        /// \brief Getter for the file name
+        /// \return 'string' which is the file name
+        std::string getFile() const;
         
+        /// \brief Function which prints the table
         void print() const;
+
+        /// \brief Function which saves the table to specific file
+        /// \param 'string' which is the file name
         void saveToFile(const std::string&) const;
 
+        /// \brief 'select' function
+        /// \param first 'const unsigned int' which is column index
+        /// \param second 'const string' which is the value to be searched for
         void select(const unsigned int&, const std::string&) const;
-        void addColumn(const std::string&, const std::string&); //name, type
+
+        /// \brief 'addcolumn' function
+        /// \param first 'const string' which is the new column name
+        /// \param second 'const string' which is the new column type
+        void addColumn(const std::string&, const std::string&);
+
+        /// \brief 'update' function
+        /// \param first 'const unsigned int' which is the asked column index
+        /// \param second 'const string' which is the asked value in row
+        /// \param third 'const unsigned int' which is the target column index
+        /// \param forth 'const string' which is the value to be assigned to corresponding cells
         void update(const unsigned int&, const std::string&, const unsigned int&, const std::string&);
+
+        /// \brief 'delete' function
+        /// \param first 'const unsigned int' which is the desired column index
+        /// \param second 'const string' which is the value to be searched for
         void del(const unsigned int&, const std::string&);
+
+        /// \brief 'insert' function
+        /// \param 'const vector<string>' which contains the values to be added to the columns
         void insert(const std::vector<std::string>&);
+
         //innerjoin
+
+        /// \brief 'rename' function
+        /// \param 'const string' which is the new name for the table
         void rename(const std::string&);
+
+        /// \brief 'count' function
+        /// \param first 'const unsigned int' which is the asked column index
+        /// \param second 'const string' which is the searched value
+        /// \return 'unsigned int' which is the number of those cells
         unsigned int count(const unsigned int&, const std::string&);
+        
         //aggregate
 
 };
